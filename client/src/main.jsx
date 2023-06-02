52ba7bde9cab0980c71be4012d7bbc7ab3b2c3c8
Darshan <bennurdarshan@gmail.com>
2023-06-02 20:37:25 +0530

    project upload

---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { Battleground, CreateBattle, Battle, Home, JoinBattle } from './page';
+import { OnboardModal } from './components';
+import { GlobalContextProvider } from './context';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <GlobalContextProvider>
+      <OnboardModal />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/battleground" element={<Battleground />} />
+        <Route path="/battle/:battleName" element={<Battle />} />
+        <Route path="/create-battle" element={<CreateBattle />} />
+        <Route path="/join-battle" element={<JoinBattle />} />
+      </Routes>
+    </GlobalContextProvider>
+  </BrowserRouter>,
+);
---
+++ client/src/page/index.js
@@ -0,0 +1,13 @@
+import Home from './Home';
+import Battle from './Battle';
+import Battleground from './Battleground';
+import CreateBattle from './CreateBattle';
+import JoinBattle from './JoinBattle';
+
+export {
+  Battle,
+  Home,
+  Battleground,
+  CreateBattle,
+  JoinBattle,
+};
